d7191f5238bd797ebbe3f4b35544a2fb1d6f5487
Slan <snchong.slan@gmail.com>
2022-05-05 19:14:29 +0800

    DestructuringAssignment

---
+++ DestructuringAssignment.js
@@ -0,0 +1,21 @@
+const Destructing = (arr) => {
+  [single] = arr;
+  [...all] = arr;
+  all2 = arr;
+  [one, two, ...other] = arr;
+
+  console.log(single, arr, all, all2, one, other);
+  console.log(
+    typeof single,
+    typeof arr,
+    typeof all,
+    typeof all2,
+    typeof one,
+    typeof other
+  );
+};
+Destructing([1, 2, 3, 4]);
+
+({ a, b, ...other } = { a: 1, b: "2", c: 3, d: "4", e: true }); //must be a,b
+console.log(a, b, other);
+console.log(typeof a, typeof b, typeof other, typeof other.e);
