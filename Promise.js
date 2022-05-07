71a6312afade8a465e3c935e9d2f54c4b8f138a9
Slan <snchong.slan@gmail.com>
2022-05-07 23:41:30 +0800

    update

--- Promise.js
+++ Promise.js
@@ -1,14 +1,34 @@
-const randomTimeOut = (resolve, reject) => {
-  let timeOut = Math.random();
-  console.log("set timeout to: " + timeOut + " seconds.");
-  setTimeout(() => {
-    if (timeOut < 0.5) {
-      console.log("call resolve()...");
-      resolve("200 OK");
-    } else {
-      console.log("call reject()...");
-      reject("timeout in " + timeOut + " seconds.");
+const userInfo = [
+  { userName: "Alice", birth: "1999-12-04" },
+  { userName: "Slan", birth: "1999-12-20" },
+  { userName: "Foo", birth: "2020-12-21" },
+  { userName: "Elon Musk", birth: "1971-06-08" },
+];
+
+const getBirth = (Name) => {
+  for (let user of userInfo) {
+    if (Name === user.userName) {
+      return new Promise((resolve, reject) => {
+        resolve(user.birth);
+      });
     }
-  }, timeOut * 1000);
+  }
 };
-randomTimeOut();
+const currentAge = (birth) => {
+  const today = new Date();
+  const birthDate = new Date(birth);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const month = today.getMonth() - birthDate.getMonth();
+  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+};
+const virtualAge = (birth) => {
+  const today = new Date();
+  const birthDate = new Date(birth);
+  return today.getFullYear() - birthDate.getFullYear();
+};
+
+console.log(getBirth("Elon Musk", currentAge));
+console.log(getBirth("Elon Musk", virtualAge));
