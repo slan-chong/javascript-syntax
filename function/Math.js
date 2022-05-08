19e2bcd39f0367f559b001792fe18a007d3fbe00
Slan <snchong.slan@gmail.com>
2022-05-09 01:14:00 +0800

    Math

---
+++ function/Math.js
@@ -0,0 +1,14 @@
+let arr = [-100, -9, 200, 150, 0];
+console.log(`Math.PI:${Math.PI}`);
+console.log(`Math.abs(-16):${Math.abs(-16)}`);
+console.log(`Math.min(arr):${Math.min(...arr)}`); //ES6
+console.log(`Math.max(arr):${Math.max(...arr)}`); //ES6
+console.log(`Math.round(4.5):${Math.round(4.5)}`);
+console.log(`Math.round(4.49):${Math.round(4.49)}`);
+console.log(`Math.pow(2, 10):${Math.pow(2, 10)}`);
+console.log(`Math.pow(125, 1/3):${Math.pow(125, 1 / 3)}`);
+console.log(`Math.floor(4.87):${Math.floor(4.87)}`);
+console.log(`Math.ceil(4.87):${Math.ceil(4.87)}`);
+console.log(`Math.floor(-4.87):${Math.floor(-4.87)}`);
+console.log(`Math.ceil(-4.87):${Math.ceil(-4.87)}`);
+console.log(`Math.random()*10:${Math.random() * 10}`);
