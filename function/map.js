6d7930f04be3cc1adf6d1523ddfb0650cdecf3e2
Slan <snchong.slan@gmail.com>
2022-05-09 21:40:15 +0800

    Map() .reduce

--- function/map.js
+++ function/map.js
@@ -1,7 +1,34 @@
+/**適合用 Map 時機
+ * key 或 value 需要順序時候
+ * key 需要不同型別時
+ * 找東西時，Map 比 Object 還快
+ * The Map object holds key-value pairs and remembers the original insertion order of the keys.
+ * Any value (both objects and primitive values) may be used as either a key or a value.
+ */
 let arr = [5, 8, 3, 2, 8, 9, 5, 4, 6, 3, 2, 1, 0, 0, 1, 1, 3, 7];
 arr.sort((a, b) => a - b);
-let map = new Map();
-for (let num of arr) {
-  map.set(num, (map.get(num) || 0) + 1);
-}
-console.log(map);
+
+//Grouping Map() usage (.map get the each val into Map())
+let group = new Map();
+arr.map((num) => {
+  group.set(num, (group.get(num) || 0) + 1);
+});
+
+console.log(group);
+console.log([...group]); //convert to array
+console.log(...group.keys());
+console.log(group.values());
+//HashMap .map usage (convert to key:val pair)
+let myObj = [
+  {
+    key: "foo",
+    val: "bar",
+  },
+  {
+    key: "hello",
+    val: "world",
+  },
+];
+const hashMap = new Map(myObj.map(({ key, val }) => [key, val]));
+console.log(hashMap);
+console.log([...hashMap]); //convert to array
--- function/reduce.js
+++ function/reduce.js
@@ -1,6 +1,39 @@
-let nums = [1, 2, 3, 4, 5, 6];
+const arr = [5, 8, 3, 2, 8, 9, 5, 4, 6, 3, 2, 1, 0, 0, 1, 1, 3, 7];
+arr.sort((a, b) => a - b);
 
-let total = nums.reduce((accu, curr) => {
+//Grouping .reduce usage (.map get the each val and using .reduce put it into num[val])
+let group = arr
+  .map((num) => num) //.map get the each val
+  .reduce((key, val) => {
+    key[val] = key[val] ? ++key[val] : 1; //key[val]內有val嗎?True的話_該key[val]++ : False_的話key[val] init to 1;
+    return key;
+  }, {}); //start with empty{}
+console.log("group:" + JSON.stringify(group));
+console.log("Keys:" + Object.keys(group));
+console.log("Values:" + Object.values(group));
+
+//HashMap .reduce usage (convert to key:val pair)
+// let pair = { [key]: val };
+const myObj = [
+  {
+    key: "foo",
+    val: "bar",
+  },
+  {
+    key: "hello",
+    val: "world",
+  },
+];
+const hashMap = myObj.reduce((map, obj) => {
+  map[obj.key] = obj.val;
+  return map;
+}, {});
+console.log(hashMap);
+console.log("Keys:" + Object.keys(hashMap));
+console.log("Values:" + Object.values(hashMap));
+
+//accumulator .reduce usage
+let total = arr.reduce((accu, curr) => {
   return accu + curr;
 }, 0);
 console.log(total);
